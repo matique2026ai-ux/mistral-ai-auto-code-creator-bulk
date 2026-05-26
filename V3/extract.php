5bb0a0d3754b6015f761465d2e41679102ad3f9f
lostandnotfoundforever <lostandnotfoundforever@gmail.com>
2026-05-26 02:16:38 +0100

    Nettoyage projet : suppression V2, V3, fichiers orphelins racine, .gitignore

--- V3/extract.php
+++
@@ -1,7 +0,0 @@
-<?php
-$html = file_get_contents('C:/Users/PCIB/Desktop/mistral-ai-auto-code-creator-bulk/V3/index.php');
-preg_match_all('/<script>(.*?)<\/script>/s', $html, $matches);
-foreach($matches[1] as $i => $js) {
-    file_put_contents('temp_' . $i . '.js', $js);
-}
-echo "Extracted.\n";
--- V3/test_include.php
+++
@@ -1,8 +0,0 @@
-<?php
-ob_start();
-include 'index.php';
-$output = ob_get_clean();
-echo 'Length: ' . strlen($output) . "\n";
-if (strlen($output) > 0) {
-    echo 'First 200 chars: ' . substr($output, 0, 200) . "\n";
-}
